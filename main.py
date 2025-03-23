0dd2dbc7ac6313580b25bc5ed519607d3d3371c1
ldav99 <davidson.139@wright.edu>
2025-03-23 15:18:47 -0400

    Added main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('data/train.csv')
+def main():
+    df = pd.read_csv('data/train.csv')
 
-print(df.head())
-print(df.info())
+    print(df.head())
+    print(df.info())
+
+if __name__=="__main__":
+    main()
