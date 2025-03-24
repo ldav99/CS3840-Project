7322065a60add439962c41e6b2ff79c63b774086
ldav99 <davidson.139@wright.edu>
2025-03-24 11:48:44 -0400

    Added code for future graphs, and functions file

---
+++ functions.py
@@ -0,0 +1,5 @@
+#Monica Brutto, Luke Davidson, Sam Webster
+#CS-3840
+#Dr. Wen Zhang 
+#Final Project
+
--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+#Monica Brutto, Luke Davidson, Sam Webster
+#CS-3840
+#Dr. Wen Zhang 
+#Final Project
+
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import functions
 
 def main():
     df = pd.read_csv('data/train.csv')
 
     print(df.head())
     print(df.info())
+
+#Call external functions
+#---------------------------------------
+
+
+
+#Results
+#---------------------------------------
+    #Plot Accuracy
+    #plt.plot(data)
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.title("Accuracy")
+    #plt.show()
+
+    #Plot Precision and Recall
+    #Google says these are one graph
+    #plt.plot(data)
+    plt.ylabel('Precision')
+    plt.xlabel('Recall')
+    plt.title("Precision-Recall")
+    #plt.show()
+
+
 
 if __name__=="__main__":
     main()
