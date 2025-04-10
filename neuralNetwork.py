c4611992a0f6194a7317ff4c697a56e86f7372ea
Sam Webster <97482477+swebster007@users.noreply.github.com>
2025-04-10 16:48:58 -0400

    Update neuralNetwork.py
    changes!

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -10,35 +10,19 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
 class NeuralNetwork(nn.Module):
-    """
-    Neural Network model for binary classification.
-    This model has three fully connected layers with ReLU activation and Dropout in between.
-    """
     def __init__(self, size):
-        """
-        Initialize the neural network.
-        
-        Arguments:
-        size (int): The number of input features (input size).
-        """
         super().__init__()
-        
         self.linear_sigmoid_stack = nn.Sequential(
             nn.Linear(size, 8),    # Input layer
             nn.BatchNorm1d(8),
             nn.ReLU(),
-            nn.Dropout(p=0.2),     # Dropout after first activation
+
 
             nn.Linear(8, 4),       # Hidden layer
             nn.BatchNorm1d(4),
-            nn.ReLU(),
-            nn.Dropout(p=0.1),     # Dropout after first activation
+            nn.ReLU(),# Dropout after first activation
 
-            nn.Linear(4, 2),
-            nn.BatchNorm1d(2),
-            nn.ReLU(),
-                            # Output layer (binary classification)
-            nn.Linear(2, 1),
+            nn.Linear(4, 1),       # Output layer (logits)
         )
 
     def forward(self, x):
